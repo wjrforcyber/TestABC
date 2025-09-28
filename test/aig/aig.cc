63f7cd7c3e784942650a37a3c128a71f5c8dcc71
JingrenWang <wjrforcyber@163.com>
2025-09-28 13:27:12 +0800

    Test(AIG): Basic construct
    Signed-off-by: JingrenWang <wjrforcyber@163.com>

---
+++ test/aig/aig.cc
@@ -0,0 +1,57 @@
+#include "gtest/gtest.h"
+
+#include "base/abc/abc.h"
+#include "misc/util/abc_namespaces.h"
+#include "misc/vec/vecPtr.h"
+
+ABC_NAMESPACE_IMPL_START
+
+/*!
+ \brief Construct a 2 input and 1 output AIG(A single AND node)
+*/
+TEST(AigTest, ConstructAig) {
+    Abc_Ntk_t * pNtk = Abc_NtkAlloc(ABC_NTK_STRASH, ABC_FUNC_AIG, 1);
+    Vec_Ptr_t * vNodes = Vec_PtrAlloc(100);
+    int nInputs = 2;
+    int nLatches = 0;
+    int nAnds = 1;
+    int nOutputs = 1;
+    int i;
+    Abc_Obj_t * pObj, * pNodeAttach;
+    vNodes = Vec_PtrAlloc( 1 + nInputs + nLatches + nAnds );
+    Vec_PtrPush( vNodes, Abc_ObjNot( Abc_AigConst1(pNtk) ) );
+
+    // create the PIs
+    for ( i = 0; i < nInputs; i++ )
+    {
+        pObj = Abc_NtkCreatePi(pNtk);    
+        Vec_PtrPush( vNodes, pObj );
+    }
+    // create the POs
+    for ( i = 0; i < nOutputs; i++ )
+    {
+        pObj = Abc_NtkCreatePo(pNtk);   
+    }
+    //create ANDs
+    for ( i = 0; i < nAnds; i++ )
+    {
+        assert( Vec_PtrSize(vNodes) == i + 1 + nInputs + nLatches );
+        Abc_Obj_t * pi00 = (Abc_Obj_t *)Vec_PtrEntry(vNodes, 1);
+        Abc_Obj_t * pi01 = (Abc_Obj_t *)Vec_PtrEntry(vNodes, 2);
+        Vec_PtrPush( vNodes, Abc_AigAnd((Abc_Aig_t *)pNtk->pManFunc, pi00, pi01) );
+    }
+    Abc_NtkForEachPo( pNtk, pObj, i )
+    {
+        pNodeAttach = Abc_ObjNotCond( (Abc_Obj_t *)Vec_PtrEntry(vNodes, 2), 1 );
+        Abc_ObjAddFanin( pObj, pNodeAttach );
+    }
+    EXPECT_TRUE(Abc_NtkPiNum(pNtk) == nInputs);
+    EXPECT_TRUE(Abc_NtkPoNum(pNtk) == nOutputs);
+    // 1 const, 2 inputs, 1 and, 1 output = 5
+    EXPECT_TRUE(Abc_NtkObjNum(pNtk) == nInputs + 1 + 1 + nOutputs);
+    EXPECT_TRUE(Abc_NtkNodeNum(pNtk) == 1);
+    Vec_PtrFree(vNodes);
+    Abc_NtkDelete(pNtk);
+}
+
+ABC_NAMESPACE_IMPL_END
