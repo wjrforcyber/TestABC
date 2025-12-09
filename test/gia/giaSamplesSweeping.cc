8af446f0ec375d5eb5a2c6fc5b1d8cf3d0af6790
JingrenWang <wjrforcyber@163.com>
2025-12-09 15:15:00 +0800

    Test(Sampling): Choice network construction
    Signed-off-by: JingrenWang <wjrforcyber@163.com>

---
+++ test/gia/giaSamplesSweeping.cc
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+
+#include "aig/aig/aig.h"
+#include "aig/gia/gia.h"
+#include "misc/util/abc_global.h"
+
+#include "proof/dch/dch.h"
+
+ABC_NAMESPACE_IMPL_START
+
+/*!
+  \brief Construct choice network using 3 snapshots from `Gia` network.
+*/
+TEST(GiaSamples, FormChoicesWith3Samples) {
+    extern Gia_Man_t * Gia_ManAigSynch2Choices( Gia_Man_t * pGia1, Gia_Man_t * pGia2, Gia_Man_t * pGia3, Dch_Pars_t * pPars );
+    Dch_Pars_t * pParsDch = (Dch_Pars_t *)malloc(sizeof(Dch_Pars_t));
+    Dch_ManSetDefaultParams( pParsDch );
+    pParsDch->nBTLimit = 100;
+    Gia_Man_t * pGia0, * pGia1, * pGia2;
+    pGia0 = Gia_AigerRead( "../data/i10Samples/i10_SpId_0.aig", 0, 0, 0 );
+    pGia1 = Gia_AigerRead( "../data/i10Samples/i10_SpId_1.aig", 0, 0, 0 );
+    pGia2 = Gia_AigerRead( "../data/i10Samples/i10_SpId_2.aig", 0, 0, 0 );
+    EXPECT_TRUE(pGia0 != nullptr);
+    EXPECT_TRUE(pGia1 != nullptr);
+    EXPECT_TRUE(pGia2 != nullptr);
+    Gia_Man_t * pFinal  = Gia_ManAigSynch2Choices( pGia0, pGia1, pGia2, pParsDch);
+    Gia_ManStop( pGia0 );
+    Gia_ManStop( pGia1 );
+    Gia_ManStop( pGia2 );
+    ABC_FREE(pParsDch);
+    EXPECT_TRUE(pFinal != nullptr);
+    EXPECT_TRUE(Gia_ManHasChoices(pFinal) == 1);
+}
+
+ABC_NAMESPACE_IMPL_END
