fdb39515851ed64fec8e93a19900b0dafffae15e
JingrenWang <wjrforcyber@163.com>
2026-01-11 19:31:17 +0800

    Test(CutTruthSim): Truth methods
    Signed-off-by: JingrenWang <wjrforcyber@163.com>

--- test/aig/aigCut.cc
+++ test/aig/aigCut.cc
@@ -16,6 +16,7 @@ extern "C"
 #endif
 
 Cut_Man_t * Abc_NtkCuts( Abc_Ntk_t * pNtk, Cut_Params_t * pParams );
+void Abc_ManResubSimulate( Vec_Ptr_t * vDivs, int nLeaves, Vec_Ptr_t * vSims, int nLeavesMax, int nWords );
 void Abc_Start();
 void Abc_Stop();
 typedef struct Abc_Frame_t_ Abc_Frame_t;
@@ -297,4 +298,126 @@ TEST(AigCutTest, CutLeavesSizeCollectReal) {
 }
 
 
+/*!
+  \brief A corner case test for cut truth computation
+*/
+TEST(AigCutTest, CutTruthSimTruth) {
+    Abc_Ntk_t * pNtk = Abc_NtkAlloc(ABC_NTK_STRASH, ABC_FUNC_AIG, 1);
+    // create the PIs
+    int nInputs = 3;
+    int nOutputs = 1;
+    int i = 0;
+    Abc_Obj_t * pObj;
+    // create the PIs
+    for ( i = 0; i < nInputs; i++ )
+    {
+        pObj = Abc_NtkCreatePi(pNtk);    
+    }
+    // create the POs
+    for ( i = 0; i < nOutputs; i++ )
+    {
+        pObj = Abc_NtkCreatePo(pNtk);   
+    }
+    Abc_Obj_t * pi_n669 = Abc_NtkPi(pNtk, 0);
+    Abc_ObjAssignName(pi_n669, "n669", NULL);
+    Abc_Obj_t * pi_n670 = Abc_NtkPi(pNtk, 1);
+    Abc_ObjAssignName(pi_n670, "n670", NULL);
+    Abc_Obj_t * pi_n675 = Abc_NtkPi(pNtk, 2);
+    Abc_ObjAssignName(pi_n675, "n675", NULL);
+    
+    Abc_Obj_t * pN676 = Abc_AigAnd((Abc_Aig_t *)pNtk->pManFunc, pi_n670, Abc_ObjNot(pi_n675));
+    Abc_ObjAssignName(pN676, "n676", NULL);
+    Abc_Obj_t * pN677 = Abc_AigAnd((Abc_Aig_t *)pNtk->pManFunc, Abc_ObjNot(pi_n670), pi_n675);
+    Abc_ObjAssignName(pN677, "n677", NULL);
+    Abc_Obj_t * pN678 = Abc_AigAnd((Abc_Aig_t *)pNtk->pManFunc, Abc_ObjNot(pN676), Abc_ObjNot(pN677));
+    Abc_ObjAssignName(pN678, "n678", NULL);
+    Abc_Obj_t * pN682 = Abc_AigAnd((Abc_Aig_t *)pNtk->pManFunc, pi_n669, Abc_ObjNot(pN678));
+    Abc_ObjAssignName(pN682, "n682", NULL);
+    Abc_ObjAddFanin(Abc_NtkPo(pNtk, 0), pN682);
+    EXPECT_TRUE(Abc_NtkPiNum(pNtk) == 3);
+    EXPECT_TRUE(Abc_NtkNodeNum(pNtk) == 4);
+    unsigned int uCutTruth;
+    unsigned int uSimTruth;
+    //Cut
+    Cut_Man_t * pCutMan;
+    Cut_Cut_t * pCut;
+    Cut_Params_t Params, * pParams = &Params;
+    memset( pParams, 0, sizeof(Cut_Params_t) );
+    pParams->nVarsMax  = 4;     // the max cut size ("k" of the k-feasible cuts)
+    pParams->nKeepMax  = 250;   // the max number of cuts kept at a node
+    pParams->fTruth    = 1;     // compute truth tables
+    pParams->fFilter   = 1;     // filter dominated cuts
+    pParams->fSeq      = 0;     // compute sequential cuts
+    pParams->fDrop     = 0;     // drop cuts on the fly
+    pParams->fVerbose  = 0;     // the verbosiness flag
+    pParams->nIdsMax   = Abc_NtkObjNumMax( pNtk );
+    pCutMan = Cut_ManStart( pParams );
+    // set cuts for PIs
+    Abc_NtkForEachCi( pNtk, pObj, i )
+        if ( Abc_ObjFanoutNum(pObj) > 0 )
+            Cut_NodeSetTriv( pCutMan, pObj->Id );
+    // get cuts for n682
+    pCut = (Cut_Cut_t *)Abc_NodeGetCutsRecursive( pCutMan, pN682, 0, 0 );
+    Vec_Ptr_t * vLeavesFilter = Vec_PtrAlloc(10);
+    Vec_PtrPush(vLeavesFilter, pi_n669);
+    Vec_PtrPush(vLeavesFilter, pi_n670);
+    Vec_PtrPush(vLeavesFilter, pi_n675);
+    for ( pCut = pCut->pNext; pCut; pCut = pCut->pNext )
+    {
+        if(Vec_PtrFind(vLeavesFilter, Abc_NtkObj( pNtk, pCut->pLeaves[0])) >= 0 \
+        && Vec_PtrFind(vLeavesFilter, Abc_NtkObj( pNtk, pCut->pLeaves[1])) >= 0 \
+           && Vec_PtrFind(vLeavesFilter, Abc_NtkObj( pNtk, pCut->pLeaves[2])) >= 0)
+        {
+            unsigned * pTruth = Cut_CutReadTruth(pCut);
+            printf("Cut:(0x%016x)\n", *pTruth & 0xff);
+            uCutTruth = (*pTruth) & 0xff;
+            // expected truth is 0x28 for n682
+            EXPECT_TRUE((*pTruth & 0xff) == 0x28);
+            for ( i = 0; i < (int)pCut->nLeaves; i++ )
+            {
+                Abc_Obj_t * pFanin = Abc_NtkObj( pN682->pNtk, pCut->pLeaves[i] );
+                printf("%d", Abc_ObjId(pFanin));
+                printf("(%s) ", Abc_ObjName(pFanin));
+            }
+            printf("\n");
+        }
+    }
+    Vec_PtrFree(vLeavesFilter);
+    Cut_ManStop(pCutMan);
+    //Sim
+    Vec_Ptr_t * vNodes = Vec_PtrAlloc(10);
+    Vec_PtrPush(vNodes, pi_n669);
+    Vec_PtrPush(vNodes, pi_n670);
+    Vec_PtrPush(vNodes, pi_n675);
+    Vec_PtrPush(vNodes, pN676);
+    Vec_PtrPush(vNodes, pN677);
+    Vec_PtrPush(vNodes, pN678);
+    Vec_PtrPush(vNodes, pN682);
+    int k;
+    int nVar       = 3;
+    int nBits      = (1 << nVar);
+    int nWords     = (nBits <= 32)? 1 : (nBits / 32);
+    Vec_Ptr_t *vSims = Vec_PtrAlloc( 100 );
+    unsigned int *pInfo = ABC_ALLOC( unsigned, nWords * (100 + 1) );
+    unsigned int *pData;
+    for ( i = 0; i < 100; i++ )
+        Vec_PtrPush( vSims, pInfo + i * nWords );
+    // set elementary truth tables
+    for ( k = 0; k < nVar; k++ )
+    {
+        pData = (unsigned *)vSims->pArray[k];
+        for ( i = 0; i < nBits; i++ )
+            if ( i & (1 << k) )
+                pData[i>>5] |= (1 << (i&31));
+    }
+    Abc_ManResubSimulate( vNodes, 3, vSims, 4, nWords);
+    unsigned int uNode = (*((unsigned int *)(pN682->pData))) & 0xff;
+    uSimTruth = uNode;
+    EXPECT_TRUE(uSimTruth == 0x28);
+    EXPECT_TRUE(uCutTruth == uSimTruth);
+    Vec_PtrFree(vNodes);
+    Vec_PtrFree(vSims);
+    ABC_FREE( pInfo );
+    Abc_NtkDelete(pNtk);
+}
 ABC_NAMESPACE_IMPL_END
